95044d6a8cfc3164c3f71e75677742e997279748
Apple <apple@cc-tonys-imac.local>
2021-10-09 23:37:04 +0300

    created project

---
+++ src/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.scss';
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+
+export default HomePage;
