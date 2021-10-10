9676996ba5bcdf80177cc541660cab4d0ef4dd33
Apple <apple@cc-tonys-imac.local>
2021-10-10 17:35:55 +0300

    update shop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import {Switch,Route,Link} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
-const Cigars = () => (
-  <div>
-    <h1>
-      Cigars Page
-    </h1>
-  </div>
-)
 function App() {
   return (
     <div>
       <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route  path='/cigar' component={Cigars} />
+      <Route  exact path='/' component={HomePage} />
+      <Route  exact path='/shop' component={ShopPage} />
       </Switch>
     
     </div>
