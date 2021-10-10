55b383116f3914b6f1bbe82086837807f866b71f
Apple <apple@cc-tonys-imac.local>
2021-10-10 14:00:36 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import {Switch,Route,Link} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 
+const Cigars = () => (
+  <div>
+    <h1>
+      Cigars Page
+    </h1>
+  </div>
+)
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route  path='/cigar' component={Cigars} />
+      </Switch>
+    
     </div>
   );
 }
